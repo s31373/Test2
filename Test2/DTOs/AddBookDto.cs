a3550c96483f728a3d656143f25cf50f3256bd60
softspheredev <98044068+softspheredev@users.noreply.github.com>
2025-06-10 11:34:21 +0200

    DTO validation

--- Test2/DTOs/AddBookDto.cs
+++ Test2/DTOs/AddBookDto.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Test2.DTOs;
 
 public class AddBookDto
 {
     public int IdPublishingHouse { get; set; }
+    [MaxLength(50)]
     public String Name { get; set; }
+    [MaxLength(50)]
     public String City { get; set; }
+    [MaxLength(50)]
     public String Country { get; set; }
     public List<int> Authors { get; set; }
     public List<int> Genres { get; set; }
